b9ac5e1cda20ccecb2ad7fb65148a7275492e60b
ealtuhova <warning.elf@gmail.com>
2018-05-24 16:58:41 +0300

    initial commit

---
+++ PSSkeleton/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PSSkeleton")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PSSkeleton")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7c4bc0a4-2939-4a19-b382-b9d090a258f9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PSSkeleton/ui/stepdefs/hooks/StepHooks.cs
@@ -0,0 +1,19 @@
+﻿using TechTalk.SpecFlow;
+
+namespace PSSkeleton.ui.stepdefs.hooks
+{
+    class StepHooks : AbstractStepDefs
+    {
+        [Before]
+        public void InitApplication()
+        {
+            BrowserFactory.LoadApplication("http://www.airmoldova.md");
+        }
+
+        [After]
+        public void CloseApplication()
+        {
+            BrowserFactory.CloseAllDrivers();
+        }
+    }
+}
